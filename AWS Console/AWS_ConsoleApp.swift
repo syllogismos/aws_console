9a2baef3bdd8752fd4f4dc16ba13eb42378a0d92
Anil Karaka <anilkaraka@live.in>
2021-05-25 18:55:40 +0530

    Initial Commit

---
+++ AWS Console/AWS_ConsoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWS_ConsoleApp.swift
+//  AWS Console
+//
+//  Created by Anil Karaka on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AWS_ConsoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
